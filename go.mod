73005b26e3eea8979ecebd85e80ac9a26ea96f4f
NB <nattakit.boonyang@gmail.com>
2025-06-21 15:32:27 +0700

    Init project

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/imkk000/go-typing-cli
+
+go 1.24
+
+require (
+	atomicgo.dev/color v0.1.0
+	atomicgo.dev/cursor v0.2.0
+	atomicgo.dev/keyboard v0.2.9
+)
+
+require (
+	github.com/containerd/console v1.0.3 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"os"
+	"strings"
+
+	"atomicgo.dev/color"
+	"atomicgo.dev/cursor"
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+)
+
+func main() {
+	textArea := cursor.NewArea()
+	text := resetArea(textArea)
+
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch key.Code {
+		case keys.CtrlC:
+			return true, nil
+		case keys.Backspace:
+			cursor.Left(1)
+			x = max(0, x-1)
+			r := rune(text[x])
+			s := string(r)
+			defaultStyle.Print(s)
+			cursor.Left(1)
+		case keys.RuneKey, keys.Space:
+			k := key.Runes[0]
+			r := rune(text[x])
+			s := string(r)
+			if r == k {
+				correctStyle.Print(s)
+			} else {
+				incorrectStyle.Print(s)
+			}
+			x = min(len(text), x+1)
+
+			if key.Code == keys.Space && current < x {
+				wordCount++
+				textArea.StartOfLine()
+				fmt.Printf("%-3d", wordCount)
+				textArea.Move(x+1, 0)
+				current = x
+			}
+		}
+		if x >= len(text) {
+			wordCount++
+			text = resetArea(textArea)
+		}
+
+		return false, nil
+	})
+}
+
+func randomWords() []string {
+	content, err := os.ReadFile("words.json")
+	if err != nil {
+		log.Fatal("read file:", err)
+	}
+	var data []string
+	if err := json.Unmarshal(content, &data); err != nil {
+		log.Fatal("unmarshal:", err)
+	}
+	var randomWord func() string
+	randomWord = func() string {
+		i := rand.IntN(len(data))
+		if _, ok := dedupedWords[data[i]]; ok {
+			return randomWord()
+		}
+		dedupedWords[data[i]] = struct{}{}
+		return data[i]
+	}
+
+	words := make([]string, maxWords)
+	for i := range maxWords {
+		words[i] = randomWord()
+	}
+	return words
+}
+
+func resetArea(a cursor.Area) string {
+	words := randomWords()
+	text := strings.Join(words, " ")
+	a.Clear()
+	a.Update(defaultStyle.Sprintf("%-3d %s", wordCount, text))
+	a.StartOfLine()
+	a.Move(5, 0)
+	x = 0
+	current = 0
+	return text
+}
+
+var (
+	x              int
+	current        int
+	wordCount      int
+	dedupedWords   = make(map[string]struct{})
+	defaultStyle   = color.NewStyle(color.ANSICyan, color.NoColor)
+	correctStyle   = color.NewStyle(color.ANSIBlack, color.ANSIYellow)
+	incorrectStyle = color.NewStyle(color.ANSICyan, color.ANSIRed)
+)
+
+const maxWords = 5
